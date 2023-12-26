f6b3b78f9d4e663b677e91076dbf48f408f67011
Lenore Chen <lenorechen@Lenores-Mac-Air-m2.local>
2023-12-26 12:42:59 -0500

    get timezone from client

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, request, jsonify, session, redirect, url_for, render_template 
-# from google.oauth2 import service_account, credentials
+import spacy
 import json
 from datetime import datetime, timedelta
+from parsedatetime import Calendar
+from dateutil import parser, tz
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from datetime import datetime, timedelta
@@ -10,6 +12,8 @@ from google_auth_oauthlib.flow import Flow
 from googleapiclient.discovery import build
 from datetime import datetime
 from dateutil import parser
+import traceback
+
 
 
 
@@ -17,12 +21,12 @@ app = Flask(__name__)
 
 # Replace with your own Google Calendar API credentials
 SCOPES = ['https://www.googleapis.com/auth/calendar']
-SERVICE_ACCOUNT_FILE = '../timemate-408921-036bf6bcf7d4.json'
 # Set the redirect URI for Google's OAuth callback
 REDIRECT_URI = 'https://127.0.0.1:5000/oauth2callback'
 
 app.secret_key = os.urandom(24)
-
+# load spacy nlp model
+nlp = spacy.load("en_core_web_sm")
 # Create a Google Calendar API service
 credentials = None
 
@@ -33,34 +37,6 @@ def create_google_calendar_service(credentials):
 def index():
     return 'Welcome to Rocketbrew Calendar Integration -- TimeMate!'
 
-
-@app.route('/create-event', methods=['POST'])
-def create_google_calendar_event():
-    data = request.get_json()
-
-    # Extract date and time information from the text input (you may need a more sophisticated parsing logic)
-    date_string = data.get('date')
-    parsed_date = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
-
-    # Create a Google Calendar event
-    event = {
-        'summary': 'Example Event',
-        'description': 'Conversation transition to a call',
-        'start': {
-            'dateTime': parsed_date.isoformat(),
-            'timeZone': 'UTC',
-        },
-        'end': {
-            'dateTime': (parsed_date + timedelta(hours=1)).isoformat(),
-            'timeZone': 'UTC',
-        },
-    }
-
-    calendar_service.events().insert(calendarId='primary', body=event).execute()
-
-    return jsonify(event)
-    # return jsonify({"message": "Google Calendar event created successfully"}), 200
-#
 @app.route('/authorize')
 def authorize():
     flow = Flow.from_client_secrets_file(
@@ -90,6 +66,8 @@ def oauth2callback():
         return redirect(url_for('prompt_additional_info'))
 
     except Exception as e:
+        # Print the stack trace
+        traceback.print_exc()
         return jsonify({"error": str(e)}), 400   
 
 @app.route('/prompt-additional-info')
@@ -105,44 +83,85 @@ def process_additional_info():
         # Retrieve additional information from the form submission
         user_text_input = request.form.get('text_input')
 
-
         # Use the saved credentials from the session
         credentials_json = session.get('credentials')
 
         if not credentials_json:
             return jsonify({"error": "User credentials not found"}), 400
-        # print(credentials)
         creds = Credentials.from_authorized_user_info(
             json.loads(credentials_json), SCOPES
         )
-        # print(creds)
-        # creds = create_credentials(credentials_json)
-        # # Build the Calendar service using the obtained credentials
+         # Parse dates using spaCy
+        parsed_date = extract_dates_with_spacy(user_text_input)
+        # Build the Calendar service using the obtained credentials
         calendar_service = build('calendar', 'v3', credentials=creds )
            # Parse the user input date to a datetime object
-        start_datetime = datetime.strptime(user_text_input.strip(), '%Y-%m-%d')
+        # Create calendar events for parsed dates
 
-        # Calculate end datetime by adding 30 minutes
-        end_datetime = start_datetime + timedelta(minutes=30)
-
-        # Set default values for summary and description
-        summary = 'Default Event Summary'
-        description = 'Default Event Description'
-
-        # Create an event
-        event = {
-            'summary': summary,
-            'description': description,
-            'start': {'dateTime': start_datetime.isoformat(), 'timeZone': 'UTC'},
-            'end': {'dateTime': end_datetime.isoformat(), 'timeZone': 'UTC'},
-        }
-
-        # Insert the event into the primary calendar
-        calendar_service.events().insert(calendarId='primary', body=event).execute()
-
+        # Retrieve the user's timezone
+        user_timezone = request.form.get('user_timezone')
+        # for date_str in parsed_dates:
+        create_calendar_event(calendar_service, parsed_date, "Meeting", "Powered by TimeMate", user_timezone)
         return "Additional information processed and event created successfully!"
 
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+
+def create_calendar_event(calendar_service, extracted_date, summary, description, user_timezone):
+
+    # extracted_date = extract_dates_with_spacy(date_str)
+
+    parsed_date = None
+    if extracted_date:
+        parsed_date = parse_relative_date(extracted_date)
+    if not parsed_date:
+        print("No valid date found")
+        return jsonify({"error": "No valid date found"}), 400
+        # Set the timezone to the user's local timezone
+    local_timezone = tz.gettz(user_timezone)
+
+    # Convert the parsed date to the user's local timezone
+    parsed_date_local = parsed_date.replace(tzinfo=local_timezone)
+
+    event = {
+        "summary": summary,
+        "description": description,
+        "start": {
+            "dateTime": parsed_date_local.isoformat(),
+            "timeZone": user_timezone,
+        },
+        "end": {
+            "dateTime": (parsed_date_local + timedelta(hours=1)).isoformat(),
+            "timeZone": user_timezone,
+        },
+    }
+
+    # Insert the event into the primary calendar
+    calendar_service.events().insert(calendarId="primary", body=event).execute()
+
+
+def extract_dates_with_spacy(user_input):
+    # import ipdb; ipdb.set_trace()
+    doc = nlp(user_input)
+    
+    # Extract entities recognized as dates
+    dates = [ent.text for ent in doc.ents if ent.label_ == "DATE"]
+    
+    if dates:
+        # Assuming the first date found is the most relevant
+        return dates[0]
+    else:
+        return None
+
+def parse_relative_date(date_text):
+    if date_text is None:
+        return None
+    cal = Calendar()
+    parsed_result, success = cal.parseDT(date_text)
+    
+    if success:
+        return parsed_result
+    else:
+        return None
 if __name__ == '__main__':
     app.run(ssl_context = 'adhoc', debug=True)
